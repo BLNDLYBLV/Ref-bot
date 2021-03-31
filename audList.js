e501877743623c91ed8b15f399662c83cb459dc6
BLNDLYBLV <sriram01201@gmail.com>
2021-03-31 16:50:57 +0530

    add new audios

--- audList.js
+++ audList.js
@@ -39,5 +39,49 @@ module.exports = listOfAudio = [
         path: "kanaadi_thirupuna.mp3",
         author: "Sriram",
     },
-    
+    {
+        tag: "thooki_adichiduven",
+        path: "thooki_adichiduven.mp3",
+        author: "Sriram",
+    },
+    {
+        tag: "indha_polapuku",
+        path: "indha_polapuku.mp3",
+        author: "Sriram",
+    },
+    {
+        tag: "indha_ranakalathula",
+        path: "indha_ranakalathula.mp3",
+        author: "Sriram",
+    },
+    {
+        tag: "namma_paya",
+        path: "namma_paya.mp3",
+        author: "Sriram",
+    },
+    {
+        tag: "sathiya_sodhanai",
+        path: "sathiya_sothanai.mp3",
+        author: "Sriram",
+    },
+    {
+        tag: "teacher_ah_vachirukaan",
+        path: "teacher_ah_vachirukaan.mp3",
+        author: "Sriram",
+    },
+    {
+        tag: "thank_you_your",
+        path: "thankyou_your.mp3",
+        author: "Sriram",
+    },
+    {
+        tag: "ulaga_nadipu",
+        path: "ulaga_nadipu.mp3",
+        author: "Sriram",
+    },
+    {
+        tag: "vela_irukku",
+        path: "vela_irukku.mp3",
+        author: "Sriram",
+    },
 ]
--- src/commands.js
+++ src/commands.js
@@ -1,12 +1,27 @@
 const {Client, Message, TextChannel} = require('discord.js');
 const listOfAudio = require('../audList');
 
+// `this is the of the list of dialogues:`
+var helpContent=`this is the of the list of dialogues:\n`;
+
+for(var i=0;i<listOfAudio.length;i++){
+    helpContent+=`
+        ${i+1}: ${listOfAudio[i].tag} by ${listOfAudio[i].author} 
+    `
+}
+
 module.exports =  ref_controller = async (message, args, bot) => {
     if(args[0]){
+        if(args[0]=='help'){
+            message.author.send(helpContent);
+            return;
+        }
         if(message.member.voice.channel){
+            var exist=false;
             var dispatcher;
             for(var i=0;i<listOfAudio.length;i++){
                 if(listOfAudio[i].tag==args[0]){
+                    exist=true;
                     const connection = await message.member.voice.channel.join();
                     dispatcher = await connection.play('./audio/'+listOfAudio[i].path);
                     dispatcher.setVolume(1);
@@ -27,12 +42,14 @@ module.exports =  ref_controller = async (message, args, bot) => {
                 console.log('ay');
                 dispatcher.resume();
             }
-            else if(args[0]=='help'){
-                console.log('help');
+            else if(!exist){
+                message.reply("The tag does not exist!");
             }
         }
         else{
-            message.reply("You must be in a voice channel to call the command!");
+            if(args[0]!='help'){
+                message.reply("You must be in a voice channel to call the command!");
+            }
         }
     }
     else{
